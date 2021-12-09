78cf8580e713a65c114ca238001fac686d47c022
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2021-12-09 18:35:49 +0530

    Rewriting code to not depend on jQuery and also bumping it to 1.0.0

--- js/link-changer.js
+++ js/link-changer.js
@@ -1,14 +1,27 @@
 // Regex that matches and extracts the ip segments out of - ip-10-181-31-23.ec2.internal
 const LINK_MATCHER = /ip-(\d{1,3})-(\d{1,3})-(\d{1,3})-(\d{1,3}).ec2.internal/ig;
 
+// Ref - https://stackoverflow.com/a/7053197/11474419
+function ready(callback) {
+  // in case the document is already rendered
+  if (document.readyState != 'loading') callback();
+  // modern browsers
+  else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+  // IE <= 8
+  else document.attachEvent('onreadystatechange', function () {
+    if (document.readyState == 'complete') callback();
+  });
+}
+
 chrome.extension.sendMessage({}, function (response) {
-  $(document).ready(function () {
+  ready(function () {
     let observer = new MutationObserver(function (mutations) {
-      $.each(jQuery("a[href*='.ec2.internal']"), function (idx, data) {
-        let href = $(data).attr('href');
+      const nodes = document.querySelectorAll("a[href*='.ec2.internal']");
+      nodes.forEach(function (element) {
+        let href = element.getAttribute('href');
         let newHref = href.replace(LINK_MATCHER, "$1.$2.$3.$4");
-        $(data).attr('href', newHref);
-      });
+        element.setAttribute('href', newHref);
+      })
     });
 
     observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
