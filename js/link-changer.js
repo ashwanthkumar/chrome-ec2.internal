e4bd9fd946f726165eefcb9195129c442b982204
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2021-12-09 18:26:50 +0530

    Using MutationObserver since Spark now loads executor tasks via Ajax after document.ready
    Ref - https://stackoverflow.com/a/45271114/11474419

--- js/link-changer.js
+++ js/link-changer.js
@@ -1,12 +1,16 @@
 // Regex that matches and extracts the ip segments out of - ip-10-181-31-23.ec2.internal
-var LINK_MATCHER = /ip-(\d{1,3})-(\d{1,3})-(\d{1,3})-(\d{1,3}).ec2.internal/ig;
+const LINK_MATCHER = /ip-(\d{1,3})-(\d{1,3})-(\d{1,3})-(\d{1,3}).ec2.internal/ig;
 
-chrome.extension.sendMessage({}, function(response) {
-  $(document).ready(function() {
-    $.each(jQuery("a[href*='.ec2.internal']"), function(idx, data) {
-      var href = $(data).attr('href');
-      var newHref = href.replace(LINK_MATCHER, "$1.$2.$3.$4");
-      $(data).attr('href', newHref);
+chrome.extension.sendMessage({}, function (response) {
+  $(document).ready(function () {
+    let observer = new MutationObserver(function (mutations) {
+      $.each(jQuery("a[href*='.ec2.internal']"), function (idx, data) {
+        let href = $(data).attr('href');
+        let newHref = href.replace(LINK_MATCHER, "$1.$2.$3.$4");
+        $(data).attr('href', newHref);
+      });
     });
+
+    observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
   });
 });
